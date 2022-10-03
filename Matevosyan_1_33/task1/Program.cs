39c340221bc817b451b79f347cca20e32c40c931
DavidMatevosyan21D <113889057+DavidMatevosyan21D@users.noreply.github.com>
2022-10-03 14:49:23 +0300

    Add files via upload

---
+++ Matevosyan_1_33/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество жителей в государстве");
+            int residents = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите площадь (в км^2)");
+            double square = double.Parse(Console.ReadLine());
+            Console.WriteLine("Плотность составляет {0:f3} чел/км^2", residents / square);
+            Console.ReadKey();
+        }
+    }
+
+}
